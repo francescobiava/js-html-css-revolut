ce3846f9b1c4970c000b4c21151d52895e465a37
Francesco <franc.ns@gmail.com>
2019-11-09 21:27:29 +0100

    added js to close dropdown menu with click out of it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
   
-  openDropdown();
+  dropdown();
+  closeDropdown();
 
 });
 
 // FUNCTIONS
 
 // funzione al click su un li dropdown
-function openDropdown() {
+function dropdown() {
   
   $('header .nav-menu li.dropdown').click(function() { 
     if (!$(this).hasClass('active')) {
@@ -19,9 +20,18 @@ function openDropdown() {
       $('header .nav-menu .right li').removeClass('active');
       $('header .nav-menu .dropdown-content').removeClass('open');
     }
-
   });
+}
 
-
-
+// funzione per chiudere dropdown anche al click in altri punti
+function closeDropdown() {
+  
+  $('html').click(function() {
+    $('header .nav-menu .right li').removeClass('active');
+    $('header .nav-menu .dropdown-content').removeClass('open');
+  });
+   
+  $('header .nav-menu li.dropdown, header .nav-menu .dropdown-content').click(function(event){
+    event.stopPropagation();
+  });
 }
